09b2cdb83349b5ccabd8a9a2b38d7ec8bf70990e
samuellando <samuel.lando@mail.concordia.ca>
2019-10-07 12:59:15 -0400

    Getting Bisection method to run

--- numericalAnalysis/Algorithms.java
+++ numericalAnalysis/Algorithms.java
@@ -3,10 +3,10 @@ import java.lang.reflect.Method;
 class Basic {
   public static FiniteDigitDecimal bisectionMethod(Method f, FiniteDigitDecimal a, FiniteDigitDecimal b, FiniteDigitDecimal tol, int n) {
     final FiniteDigitDecimal f2 = new FiniteDigitDecimal(2, 1000);
-    FiniteDigitDecimal pn;
+    FiniteDigitDecimal pn = null;
     FiniteDigitDecimal pnmin1 = a.clone();
     for (int i = 0; i < n; i++) {
-      pn = a.add(b).divide(f2);
+      pn = b.add(a).divide(f2);
 
       if (pn.substract(pnmin1).abs().compareTo(tol) < 0)
         return pn;
@@ -21,6 +21,6 @@ class Basic {
       }
       pnmin1 = pn;
     }
-    return null;
+    return pn;
   }
 }
--- numericalAnalysis/Driver.java
+++ numericalAnalysis/Driver.java
@@ -6,19 +6,20 @@ public class Driver {
   }
 
   public static void main(String[] args) {
-    FiniteDigitDecimal a = new FiniteDigitDecimal(0.1, 5);
+    FiniteDigitDecimal a = new FiniteDigitDecimal(-1, 5);
     FiniteDigitDecimal b = new FiniteDigitDecimal(3, 5);
     FiniteDigitDecimal tol = new FiniteDigitDecimal(0.00001, 5);
+    FiniteDigitDecimal val;
     Method f = null;
     try {
       f = Driver.class.getMethod("f", a.getClass());
-      System.out.println(f.invoke(null, a));
     } catch (Exception e) {
       System.out.println("Couldn't find method f");
       System.exit(-1);
     }
-    for (int i = 1; i < 10; i++) {
-      System.out.println(Basic.bisectionMethod(f, a, b, tol, i));
+    for (int i = 1; i < 20; i++) {
+      val = Basic.bisectionMethod(f, a, b, tol, i);
+      System.out.println(val+" "+f(val));
     }
   }
 }
